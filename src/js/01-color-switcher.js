5779187b70d8b571bd176dc06185be92be1b5b71
Vera Lyamzina <lyamzina91@gmail.com>
2023-07-11 19:23:16 +0300

    переключатель цветов

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+document.addEventListener('DOMContentLoaded', function () {
+    const startButton = document.querySelector('[data-start]');
+    const stopButton = document.querySelector('[data-stop]');
+    const body = document.body;
+    let intervalId;
+
+    startButton.addEventListener('click', function () {
+        intervalId = setInterval(function () {
+            const randomColor = getRandomHexColor();
+            body.style.backgroundColor = randomColor;
+        }, 1000);
+    });
+
+    stopButton.addEventListener('click', function () {
+        clearInterval(intervalId)
+    });
+});
